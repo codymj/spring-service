c3ec80a126b86e185d88fc1422268c7d08ab94db
Cody M. Johnson <codyj@protonmail.com>
2023-07-12 11:36:20 -0400

    init service

--- src/main/java/com/codymj/springservice/SpringServiceApplication.java
+++ src/main/java/com/codymj/springservice/SpringServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringServiceApplication.class, args);
 	}
-
 }
